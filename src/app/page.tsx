5b53b9d9bc0d82b1c185e4dbe631655340445f14
Ariel <cesarzaracho2003@gmail.com>
2025-09-30 22:47:49 +0000

    haz 2 rollbacks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
             {event.artists.map((artist) => (
               <Card key={artist.id} className="overflow-hidden text-center transition-all hover:shadow-xl hover:-translate-y-2">
                 <CardHeader className="p-0">
-                  <div className="relative h-32 w-full">
+                  <div className="relative h-48 w-full">
                     <Image src={artist.imageUrl} alt={artist.name} fill className="object-cover" data-ai-hint={artist.imageHint} />
                   </div>
                 </CardHeader>
